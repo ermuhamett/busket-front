fc2606cb3bae49413935e0af405fc9278e021916
YBekbosynov <yermykhamet.bekbosynov@kazzinc.com>
2025-02-18 13:57:46 +0500

    Добавлен дополнительная логика для ковша в котором оно не используется

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,4 @@
-// Массив с путями к SVG-файлам
+// Массив с путями к SVG-файлам (ковши)
 const svgFiles = [
     "./svg/blue_bucket.svg",
     "./svg/yellow_bucket.svg",
@@ -11,71 +11,91 @@ const svgFiles = [
 // Функция для загрузки SVG по URL
 function loadSVG(url, callback) {
     $.get(url, function(data) {
-        // Из загруженных данных находим элемент <svg>
         let svg = $(data).find('svg');
         callback(svg);
     });
 }
 
-// Функция, создающая контейнер для ковша с заданным номером
-// В контейнере сразу появляется статическая подпись, а SVG вставится позже
+// Создаёт контейнер для ковша с подписью (номер)
 function createBucketContainer(id) {
-    // Создаем контейнер для SVG и подписи (номер ковша)
     let bucketContainer = $('<div class="bucket-container"></div>');
-    // Добавляем пустой элемент, куда потом вставим SVG
+    // Чтобы спаннер (с абсолютным позиционированием) располагался внутри
+    bucketContainer.css('position', 'relative');
+
     let svgPlaceholder = $('<div class="bucket-svg"></div>');
     bucketContainer.append(svgPlaceholder);
-    // Добавляем подпись с номером
+
+    // Добавляем подпись с номером (делаем жирной в CSS)
     bucketContainer.append(`<div class="bucket-id">${id}</div>`);
+
     return { bucketContainer, svgPlaceholder };
 }
 
-// Функция генерации ковшей
-// containerId  - селектор контейнера, куда будут добавляться ковши
-// bucketCount  - сколько ковшей создать
-// startId      - начальный номер (например, 101 или 201)
+// Генерирует ковши с заданной нумерацией
 function generateBuckets(containerId, bucketCount, startId) {
     let container = $(containerId);
 
     for (let i = 0; i < bucketCount; i++) {
         let bucketNumber = startId + i;
-        // Создаем контейнер с подписью (номерация статична)
         let { bucketContainer, svgPlaceholder } = createBucketContainer(bucketNumber);
-        // Добавляем контейнер сразу, чтобы нумерация была в нужном порядке
         container.append(bucketContainer);
 
-        // Выбираем случайный SVG из массива
+        // Случайный выбор одного из доступных SVG-файлов
         let randomIndex = Math.floor(Math.random() * svgFiles.length);
         let svgUrl = svgFiles[randomIndex];
 
-        // Загружаем SVG и вставляем в созданный placeholder
+        // Загружаем и вставляем SVG
         loadSVG(svgUrl, function(svgTemplate) {
             let svgClone = svgTemplate.clone();
             svgClone.attr('id', `bucket-${bucketNumber}`);
-            svgClone.addClass('bucket'); // Для стилизации и обработки кликов
+            svgClone.addClass('bucket');
             // Удаляем inline-стили, если они есть
             svgClone.find('.bucket-fill').removeAttr('style');
             svgPlaceholder.append(svgClone);
         });
     }
 
-    // Обработчик клика для переключения класса "active" у каждого SVG-ковша
+    // При клике на ковш добавляем/убираем класс active (выделение)
     container.on('click', '.bucket', function() {
         $(this).toggleClass('active');
     });
 }
 
-// Инициализация после загрузки документа
+// Накладывает spanner.svg на выбранные (активные) ковши
+function overlaySpannerOnActiveBuckets() {
+    let spannerUrl = "./svg/spanner.svg";
+    
+    loadSVG(spannerUrl, function(spannerSVG) {
+        // Для каждого ковша, у которого класс .active
+        $('.bucket.active').each(function() {
+            let $bucket = $(this);
+            let $container = $bucket.closest('.bucket-container');
+
+            // Удаляем предыдущий спаннер (если есть)
+            $container.find('.spanner-overlay').remove();
+            // Также убираем предыдущую подсветку (если вдруг была)
+            $container.removeClass('overlayed');
+
+            // Клонируем спаннер
+            let spannerClone = spannerSVG.clone().addClass('spanner-overlay');
+
+            // Добавляем его в контейнер, чтобы он располагался поверх ковша
+            $container.append(spannerClone);
+
+            // Добавляем класс, который подсвечивает ковш
+            $container.addClass('overlayed');
+        });
+    });
+}
+
 $(document).ready(function() {
-    // Генерируем 20 ковшей с нумерацией от 101 до 120 в контейнере #bucket-grid-1
+    // Генерируем 20 ковшей (101–120)
     generateBuckets('#bucket-grid-1', 20, 101);
-    // Генерируем 20 ковшей с нумерацией от 201 до 220 в контейнере #bucket-grid-2
+    // Генерируем 20 ковшей (201–220)
     generateBuckets('#bucket-grid-2', 20, 201);
 
-    // Пример обработчика для кнопки, который переключает класс "active" для всех SVG-ковшей
-    $('#set-color').on('click', function() {
-        $('.bucket').each(function() {
-            $(this).toggleClass('active');
-        });
+    // Клик по кнопке "Overlay Spanner"
+    $('#overlay-spanner').on('click', function() {
+        overlaySpannerOnActiveBuckets();
     });
 });
